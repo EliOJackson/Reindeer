f07c6445dc57922b83cafd5c15845775af589343
Eli Jackson <EliJackson@Elis-MacBook-Pro.local>
2017-12-05 15:56:43 -0600

    cleaned up JS to print reindeer and colors onto html

--- reindeer.js
+++ reindeer.js
@@ -7,9 +7,11 @@ var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donne
 var hohohoElement = document.getElementById("reindeer");
 
 for (var i = 0; i < reindeer.length; i++) {
-    var christmas = colors[i] + " " + reindeer[i];
-    console.log(christmas)
+    // var christmas = colors[i] + " " + reindeer[i];
+    // console.log(christmas)
+
+    hohohoElement.innerHTML += `<div>${colors[i]} ${reindeer[i]}</div>`;
     
 }
 
-hohohoElement.innerHTML = christmas;
+// hohohoElement.innerHTML = christmas;
