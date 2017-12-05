29ef63cc447bb30d86df13658ceadb226bb02a8c
Eli Jackson <EliJackson@Elis-MacBook-Pro.local>
2017-12-05 15:51:55 -0600

    able to print colors and reindeer to console

---
+++ reindeer.js
@@ -0,0 +1,15 @@
+
+
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var hohohoElement = document.getElementById("reindeer");
+
+for (var i = 0; i < reindeer.length; i++) {
+    var christmas = colors[i] + " " + reindeer[i];
+    console.log(christmas)
+    
+}
+
+hohohoElement.innerHTML = christmas;
